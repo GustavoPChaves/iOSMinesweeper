0dffd7c8ce694aae2872a29c69d0fd2d66e3efd5
Gustavo Chaves <gustavoportelada@gmail.com>
2022-10-31 16:24:52 -0300

    updating appdelegate

--- Minesweeper/AppDelegate.swift
+++ Minesweeper/AppDelegate.swift
@@ -6,3 +6,19 @@
 //  Copyright © 2019 Gustavo Chaves. All rights reserved.
 //
 
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = GameViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
