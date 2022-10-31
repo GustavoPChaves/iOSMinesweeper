0ff9a512c224dad8cedea24da54076cbdad8bb74
Gustavo Chaves <gustavoportelada@gmail.com>
2022-10-31 16:18:48 -0300

    FEature SKButtonNode

--- Minesweeper/Nodes/SKButtonNode.swift
+++ Minesweeper/Nodes/SKButtonNode.swift
@@ -10,3 +10,33 @@ import Foundation
 import SpriteKit
 
 
+/// Class to create a custom button
+class SKButtonNode: SKSpriteNode{
+    var label: SKLabelNode!
+    var action: (()->())?
+    init(color: UIColor, size: CGSize, position: CGPoint, text: String, action: @escaping ()->()) {
+        super.init(texture: nil, color: color, size: size)
+        let label = setupLabel(message: text, position: position)
+        label.zPosition = 1
+        self.position = position
+        addChild(label)
+        isUserInteractionEnabled = true
+        self.action = action
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        action?()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupLabel(message: String, position: CGPoint)->SKLabelNode{
+        let label = SKLabelNode(text: message)
+        label.fontColor = .black
+        label.fontSize = 18
+        label.fontName = "HelveticaNeue-Medium"
+        label.verticalAlignmentMode = .center
+        label.horizontalAlignmentMode = .center
+        return label
+    }
+}
